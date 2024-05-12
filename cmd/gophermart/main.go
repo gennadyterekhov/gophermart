9df9cd3a91ca3bc623b1615bf02ecda5e5ab929f
Gennady Terekhov <ter.ge@yandex.ru>
2024-05-12 19:55:36 +0400

    init

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	var config *Config = getConfig()
+	fmt.Printf("Server started on %v\n", config.Addr)
+	err = http.ListenAndServe(config.Addr, handlers.GetRouter())
+	if err != nil {
+		panic(err)
+	}
+}
